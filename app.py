f1e6ba1c24e7bdde5fe834853fe5f3e5e9e17d2b
Alex McCarthy <alexcomp2@outlook.com>
2025-07-04 15:41:31 +0100

    testing

--- app.py
+++ app.py
@@ -762,5 +762,5 @@ def api_node_relations():
         relations = [dict(record) for record in result]
     return jsonify(relations)
 
-#if __name__ == "__main__":
-    #app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
